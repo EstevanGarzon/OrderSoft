0aded777a43e0312fb1ec52b98ce1b5c3bc4b0aa
Estevan Garzon <estevan.garzon@hotmail.com>
2020-03-23 19:49:59 -0500

    Login

---
+++ OrderSoftV0/OrderSoftV0/Controllers/ContentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderSoftV0.Controllers
+{
+    public class ContentController : Controller
+    {
+        public ActionResult Menu()
+        {
+            return View();
+        }
+
+    }
+}
