5949049b6c7ab72c26aa8a1d3ef3c8842cc40bef
Estevan Garzon <estevan.garzon@hotmail.com>
2020-03-29 21:56:40 -0500

    Ajuste del llamado a otras paginas

--- OrderSoftV0/OrderSoftV0/Controllers/ContentController.cs
+++ OrderSoftV0/OrderSoftV0/Controllers/ContentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OrderSoftV0.Models;
 
 namespace OrderSoftV0.Controllers
 {
--- OrderSoftV0/OrderSoftV0/Controllers/HomeController.cs
+++ OrderSoftV0/OrderSoftV0/Controllers/HomeController.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OrderSoftV0.Models;
 
 namespace OrderSoftV0.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Index i)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "Bienvenid@" + i.user;
+                return Redirect("~/Content/Menu");
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            return Redirect("~/Content/Menu");
         }
 
         public ActionResult Contact()
@@ -26,5 +42,6 @@ namespace OrderSoftV0.Controllers
 
             return View();
         }
+
     }
 }
