54ddabf7c766476c9e6ae1ccb4f10b1dc2f7e4ed
Estevan Garzon <estevan.garzon@hotmail.com>
2020-05-10 12:30:37 -0500

    conexión a la base de datos

--- OrderSoftV0/OrderSoftV0/Controllers/HomeController.cs
+++ OrderSoftV0/OrderSoftV0/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,6 +10,8 @@ namespace OrderSoftV0.Controllers
 {
     public class HomeController : Controller
     {
+        SqlConnection connection = new SqlConnection("server=LAPTOP-5PQE6DUI\\SQLEXPRESS; database=OrderSoft; user=LAPTOP-5PQE6DUI\\estev; password=; integrated security = true");
+
         [HttpGet]
         public ActionResult Index()
         {
@@ -18,13 +21,52 @@ namespace OrderSoftV0.Controllers
         [HttpPost]
         public ActionResult Index(Index i)
         {
-            if (ModelState.IsValid)
+            string psw = "";
+            try
             {
-                ViewBag.Message = "Bienvenid@" + i.user;
-                return Redirect("~/Content/Menu");
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("select psw from dbo.view_usuarios where cedula="+i.user+";", connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        psw = reader.GetString(0);
+                        ViewBag.nombre = reader.GetString(0);
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "No trae información";
+                }
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Message = "Problemas de conexión - "+ex.Message;
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            if (ModelState.IsValid )
+            {
+                if (psw.Equals(i.password))
+                {
+                    return Redirect("~/Content/Menu");
+                }
+                else
+                {
+                    ViewBag.Message = "Creenciales incorrectas.";
+                    return View();
+                }
             }
             else
             {
+                ViewBag.Message = "Los campos está mal.";
                 return View();
             }
         }
---
+++ OrderSoftV0/OrderSoftV0/Models/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OrderSoftV0.Models
+{
+    public class Menu
+    {
+        public string nombre { get; set; }
+
+    }
+}
